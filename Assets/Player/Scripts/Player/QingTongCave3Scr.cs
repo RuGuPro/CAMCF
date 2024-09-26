578b341a4c77e909c3c0ec46a5aa99de0286529a
qibowen <qibw@rainier.net.cn>
2024-09-26 16:59:16 +0800

    更新青瞳手柄接收数据
    更新青瞳手柄接收数据

--- Assets/Player/Scripts/Player/QingTongCave3Scr.cs
+++ Assets/Player/Scripts/Player/QingTongCave3Scr.cs
@@ -25,7 +25,17 @@ public class QingTongCave3Scr : MonoBehaviour, UpdataCamera
                 Quaternion quaternionTemp = quaternionRTS;
                 transformObj.localEulerAngles = new Vector3(quaternionTemp.eulerAngles.x * _infoConfig.CameraInfo.deltaXRotDirection,
                                 quaternionTemp.eulerAngles.y * _infoConfig.CameraInfo.deltaYRotDirection, quaternionTemp.eulerAngles.z * _infoConfig.CameraInfo.deltaZRotDirection) + new Vector3(_infoConfig.CameraInfo.deltaRotX, _infoConfig.CameraInfo.deltaRotY, _infoConfig.CameraInfo.deltaRotZ);
-            };  
+            };
+            HandBodyTracker.UpdatePos = (transformObj, vectorRTS) =>
+            {
+                transformObj.localPosition = new Vector3(vectorRTS.x * _infoConfig.CameraInfo.deltaXDirection, vectorRTS.y * _infoConfig.CameraInfo.deltaYDirection, vectorRTS.z * _infoConfig.CameraInfo.deltaZDirection) + new Vector3(_infoConfig.CameraInfo.deltaX, _infoConfig.CameraInfo.deltaY, _infoConfig.CameraInfo.deltaZ);
+            };
+            HandBodyTracker.UpdateRot = (transformObj, quaternionRTS) =>
+            {
+                Quaternion quaternionTemp = quaternionRTS;
+                transformObj.localEulerAngles = new Vector3(quaternionTemp.eulerAngles.x * _infoConfig.CameraInfo.deltaXRotDirection,
+                                quaternionTemp.eulerAngles.y * _infoConfig.CameraInfo.deltaYRotDirection, quaternionTemp.eulerAngles.z * _infoConfig.CameraInfo.deltaZRotDirection) + new Vector3(_infoConfig.CameraInfo.deltaRotX, _infoConfig.CameraInfo.deltaRotY, _infoConfig.CameraInfo.deltaRotZ);
+            };
 
             CMPluginThreadManager.UpdateInfo(_infoConfig);
         }
--- Assets/Player/Scripts/Player/QingTongCave4Scr.cs
+++ Assets/Player/Scripts/Player/QingTongCave4Scr.cs
@@ -26,6 +26,16 @@ public class QingTongCave4Scr : MonoBehaviour, UpdataCamera
                 transformObj.localEulerAngles = new Vector3(quaternionTemp.eulerAngles.x * _infoConfig.CameraInfo.deltaXRotDirection,
                                 quaternionTemp.eulerAngles.y * _infoConfig.CameraInfo.deltaYRotDirection, quaternionTemp.eulerAngles.z * _infoConfig.CameraInfo.deltaZRotDirection) + new Vector3(_infoConfig.CameraInfo.deltaRotX, _infoConfig.CameraInfo.deltaRotY, _infoConfig.CameraInfo.deltaRotZ);
             };
+            HandBodyTracker.UpdatePos = (transformObj, vectorRTS) =>
+            {
+                transformObj.localPosition = new Vector3(vectorRTS.x * _infoConfig.CameraInfo.deltaXDirection, vectorRTS.y * _infoConfig.CameraInfo.deltaYDirection, vectorRTS.z * _infoConfig.CameraInfo.deltaZDirection) + new Vector3(_infoConfig.CameraInfo.deltaX, _infoConfig.CameraInfo.deltaY, _infoConfig.CameraInfo.deltaZ);
+            };
+            HandBodyTracker.UpdateRot = (transformObj, quaternionRTS) =>
+            {
+                Quaternion quaternionTemp = quaternionRTS;
+                transformObj.localEulerAngles = new Vector3(quaternionTemp.eulerAngles.x * _infoConfig.CameraInfo.deltaXRotDirection,
+                                quaternionTemp.eulerAngles.y * _infoConfig.CameraInfo.deltaYRotDirection, quaternionTemp.eulerAngles.z * _infoConfig.CameraInfo.deltaZRotDirection) + new Vector3(_infoConfig.CameraInfo.deltaRotX, _infoConfig.CameraInfo.deltaRotY, _infoConfig.CameraInfo.deltaRotZ);
+            };
 
             CMPluginThreadManager.UpdateInfo(_infoConfig);
         }
